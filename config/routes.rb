39c05bb110bdbf05b6ecedf9f9824d77eb42f4c4
ssenter@tut.by <ssenter@tut.by>
2014-12-03 11:39:31 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products
 
+  root 'products#index'
+
+  match '/new', to: 'products#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
